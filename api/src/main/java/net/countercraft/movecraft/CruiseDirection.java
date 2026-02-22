021596c17c55d87c4cb2a8a310d30facc72b6616
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2026-02-22 13:26:35 +0100

    respect Y component of vector too

--- api/src/main/java/net/countercraft/movecraft/CruiseDirection.java
+++ api/src/main/java/net/countercraft/movecraft/CruiseDirection.java
@@ -72,7 +72,7 @@ public class CruiseDirection extends Vector {
     /** Rise or dive (if angle is positive), angle in radians. Will default to UP (or DOWN) if risen too much. */
     // TODO @HalfQuark: Check this function again, it does not seem to work how we want it to (always results in a vector point somewhere downwards)
     public void rise2D(double angle) {
-        Vector perpendicular = new Vector(this.getX(), 0, this.getZ()).rotateAroundY(PI_HALF);
+        Vector perpendicular = new Vector(this.getX(), this.getY(), this.getZ()).rotateAroundY(PI_HALF);
         if (angle > 0) {
             angle = Math.min(angle, this.angle(UP));
         } else {
