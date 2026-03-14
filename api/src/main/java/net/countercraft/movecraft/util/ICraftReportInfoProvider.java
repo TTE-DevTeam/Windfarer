a93c28de868047fbd037d52a8ba6ea0bcf52d152
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2026-03-14 18:50:47 +0100

    add customization interface for craftreport

---
+++ api/src/main/java/net/countercraft/movecraft/util/ICraftReportInfoProvider.java
@@ -0,0 +1,11 @@
+package net.countercraft.movecraft.util;
+
+import net.kyori.adventure.text.Component;
+
+import java.util.Set;
+
+public interface ICraftReportInfoProvider {
+
+    Set<Component> getReportInfo();
+
+}
