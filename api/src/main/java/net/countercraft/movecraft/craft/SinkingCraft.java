a01f2ced8d6d341eb9c49638fbdd5d869a545e2d
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2026-03-29 18:43:12 +0200

    never auto release sinkingCrafts

--- api/src/main/java/net/countercraft/movecraft/craft/SinkingCraft.java
+++ api/src/main/java/net/countercraft/movecraft/craft/SinkingCraft.java
@@ -1,4 +1,9 @@
 package net.countercraft.movecraft.craft;
 
 public interface SinkingCraft extends Craft {
+
+    @Override
+    default boolean shouldAutoRelease(final long autoReleaseTimeout, final long maxTimeBetweenCruiseUpdates) {
+        return false;
+    }
 }
