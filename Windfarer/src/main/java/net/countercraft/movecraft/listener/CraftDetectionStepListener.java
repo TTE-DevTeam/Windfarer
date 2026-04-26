3e8bbcbb6853da45dc33ba78dc89f8c874fb5802
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2026-04-26 19:59:10 +0200

    add event to add more detection steps

---
+++ Windfarer/src/main/java/net/countercraft/movecraft/listener/CraftDetectionStepListener.java
@@ -0,0 +1,17 @@
+package net.countercraft.movecraft.listener;
+
+import net.countercraft.movecraft.events.CraftGatherAdditionalDetectionStepsEvent;
+import net.countercraft.movecraft.processing.tasks.detection.SignDetectionTask;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+
+public class CraftDetectionStepListener implements Listener {
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onDetectAddSteps(final CraftGatherAdditionalDetectionStepsEvent event) {
+        // Sign locations
+        event.addStep(new SignDetectionTask(event.getCraft()));
+    }
+
+}
