183214efc2ce7a5866017f7377cdbf1c849d570f
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2026-05-09 12:47:53 +0200

    forgot change in baseclass

--- api/src/main/java/net/countercraft/movecraft/craft/controller/directControl/AbstractDirectControlSlot.java
+++ api/src/main/java/net/countercraft/movecraft/craft/controller/directControl/AbstractDirectControlSlot.java
@@ -4,14 +4,15 @@ import net.countercraft.movecraft.CruiseDirection;
 import net.countercraft.movecraft.craft.Craft;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.function.Consumer;
 
 public abstract class AbstractDirectControlSlot implements ConfigurationSerializable {
 
-    public abstract boolean onLeftClick(ItemStack itemStack, Player interactor, Craft craft);
-    public abstract boolean onRightClick(ItemStack itemStack, Player interactor, Craft craft);
+    public abstract boolean onLeftClick(ItemStack itemStack, Player interactor, Craft craft, Action action);
+    public abstract boolean onRightClick(ItemStack itemStack, Player interactor, Craft craft, Action action);
     public abstract boolean onItemDrop(ItemStack itemStack, Player interactor, Craft craft);
     public abstract boolean onSwapHand(ItemStack itemStackMainHand, ItemStack itemStackOffHand, Player interactor, Craft craft);
     public abstract boolean onPreCruise(Player activePilot, Craft craft, int tickCooldown, Consumer<Integer> modifyTickCooldown, CruiseDirection cruiseDirection);
