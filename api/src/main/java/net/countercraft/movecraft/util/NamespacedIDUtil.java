0bdf9ac91471b451a9a8705a8acbe9a76c2a33a6
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2026-05-17 21:23:03 +0200

    fix custom blocks with craftengine

--- api/src/main/java/net/countercraft/movecraft/util/NamespacedIDUtil.java
+++ api/src/main/java/net/countercraft/movecraft/util/NamespacedIDUtil.java
@@ -14,7 +14,17 @@ public class NamespacedIDUtil {
 
     public static NamespacedKey getBlockID(final BlockData blockData) {
         if (blockData == null || !(blockData instanceof Keyed)) {
-            return blockData.getMaterial().getKey();
+            // Super ugly workaround but egh, works
+            String asString = blockData.getAsString();
+            int dataStartIndex = asString.indexOf('[');
+            if (dataStartIndex >= 0) {
+                asString = asString.substring(0, dataStartIndex);
+            }
+            if (asString.isEmpty()) {
+                return blockData.getMaterial().getKey();
+            } else {
+                return NamespacedKey.fromString(asString);
+            }
         } else {
             Keyed keyed = (Keyed) blockData;
             return keyed.getKey();
