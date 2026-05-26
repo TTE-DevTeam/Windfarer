e5f9ffb49a749bb1ce86ca6ef068d6673cf22eaa
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2026-05-26 16:53:20 +0200

    fix cooldown implementation

--- api/src/main/java/net/countercraft/movecraft/craft/controller/directControl/AbstractDirectControlSlot.java
+++ api/src/main/java/net/countercraft/movecraft/craft/controller/directControl/AbstractDirectControlSlot.java
@@ -2,6 +2,9 @@ package net.countercraft.movecraft.craft.controller.directControl;
 
 import net.countercraft.movecraft.CruiseDirection;
 import net.countercraft.movecraft.craft.Craft;
+import net.countercraft.movecraft.craft.datatag.CraftDataTagKey;
+import net.countercraft.movecraft.craft.datatag.CraftDataTagRegistry;
+import org.bukkit.NamespacedKey;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
@@ -11,19 +14,22 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
 
 public abstract class AbstractDirectControlSlot implements ConfigurationSerializable {
 
     protected final long cooldown;
-    // We have one instance per craft as this all gets cloned!
-    protected long lastUseTime;
 
     protected abstract boolean doOnLeftClick(ItemStack itemStack, Player interactor, Craft craft, Action action);
     protected abstract boolean doOnRightClick(ItemStack itemStack, Player interactor, Craft craft, Action action);
     protected abstract boolean doOnItemDrop(ItemStack itemStack, Player interactor, Craft craft);
     protected abstract boolean doOnSwapHand(ItemStack itemStackMainHand, ItemStack itemStackOffHand, Player interactor, Craft craft);
     protected abstract boolean doOnPreCruise(Player activePilot, Craft craft, int tickCooldown, Consumer<Integer> modifyTickCooldown, CruiseDirection cruiseDirection);
+
+    private static final CraftDataTagKey<Map<AbstractDirectControlSlot, Long>> COOLDOWN_MAP = CraftDataTagRegistry.INSTANCE.registerTagKey(new NamespacedKey("windfarer", "direct_control/cooldowns"), c -> new ConcurrentHashMap<>());
+
 
     public abstract AbstractDirectControlSlot clone();
 
@@ -35,67 +41,80 @@ public abstract class AbstractDirectControlSlot implements ConfigurationSerializ
         this.cooldown = NumberConversions.toLong(yamlData.getOrDefault("cooldown", 200L));
     }
 
-    protected boolean isReady() {
+    protected boolean isReady(final Craft craft) {
         if (this.cooldown <= 0) {
             return true;
         } else {
-            return this.lastUseTime < System.currentTimeMillis();
+            Optional<Long> optLastUseTime = this.getCooldown(craft);
+            if (optLastUseTime.isEmpty()) {
+                return true;
+            } else {
+                return optLastUseTime.get() < System.currentTimeMillis();
+            }
         }
     }
 
     public final boolean onLeftClick(ItemStack itemStack, Player interactor, Craft craft, Action action) {
-        if (!this.isReady()) {
+        if (!this.isReady(craft)) {
             return false;
         }
         final boolean result = this.doOnLeftClick(itemStack, interactor, craft, action);
         if (result) {
-            this.lastUseTime = System.currentTimeMillis();
+            this.setCooldown(craft);
         }
         return result;
     }
 
     public final boolean onRightClick(ItemStack itemStack, Player interactor, Craft craft, Action action) {
-        if (!this.isReady()) {
+        if (!this.isReady(craft)) {
             return false;
         }
         final boolean result = this.doOnRightClick(itemStack, interactor, craft, action);
         if (result) {
-            this.lastUseTime = System.currentTimeMillis();
+            this.setCooldown(craft);
         }
         return result;
     }
 
     public final boolean onItemDrop(ItemStack itemStack, Player interactor, Craft craft) {
-        if (!this.isReady()) {
+        if (!this.isReady(craft)) {
             return false;
         }
         final boolean result = this.doOnItemDrop(itemStack, interactor, craft);
         if (result) {
-            this.lastUseTime = System.currentTimeMillis();
+            this.setCooldown(craft);
         }
         return result;
     }
 
     public final boolean onSwapHand(ItemStack itemStackMainHand, ItemStack itemStackOffHand, Player interactor, Craft craft) {
-        if (!this.isReady()) {
+        if (!this.isReady(craft)) {
             return false;
         }
         final boolean result = this.doOnSwapHand(itemStackMainHand, itemStackOffHand, interactor, craft);
         if (result) {
-            this.lastUseTime = System.currentTimeMillis();
+            this.setCooldown(craft);
         }
         return result;
     }
 
     public final boolean onPreCruise(Player activePilot, Craft craft, int tickCooldown, Consumer<Integer> modifyTickCooldown, CruiseDirection cruiseDirection) {
-        if (!this.isReady()) {
+        if (!this.isReady(craft)) {
             return false;
         }
         final boolean result = this.doOnPreCruise(activePilot, craft, tickCooldown, modifyTickCooldown, cruiseDirection);
         if (result) {
-            this.lastUseTime = System.currentTimeMillis();
+            this.setCooldown(craft);
         }
         return result;
+    }
+
+    protected final Optional<Long> getCooldown(final Craft craft) {
+        return Optional.ofNullable(craft.getDataTag(COOLDOWN_MAP).getOrDefault(this, null));
+    }
+
+    protected void setCooldown(final Craft craft) {
+        craft.getDataTag(COOLDOWN_MAP).put(this, System.currentTimeMillis() + this.cooldown);
     }
 
     @Override
