ea09fba9969a7d66585d146aca7b1eae455839da
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2026-06-12 21:22:05 +0200

    run async task to update banners AFTER the craft finished moving

--- Windfarer/src/main/java/net/countercraft/movecraft/listener/CraftTranslateListener.java
+++ Windfarer/src/main/java/net/countercraft/movecraft/listener/CraftTranslateListener.java
@@ -1,14 +1,11 @@
 package net.countercraft.movecraft.listener;
 
-import net.countercraft.movecraft.MovecraftLocation;
 import net.countercraft.movecraft.craft.Craft;
+import net.countercraft.movecraft.events.CraftFinishMovementEvent;
 import net.countercraft.movecraft.events.CraftPreTranslateEvent;
-import net.countercraft.movecraft.events.CraftTranslateEvent;
+import net.countercraft.movecraft.processing.WorldManager;
+import net.countercraft.movecraft.processing.tasks.UpdateBannersTask;
 import net.countercraft.movecraft.util.ContactBlockHelper;
-import org.bukkit.World;
-import org.bukkit.block.Banner;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockState;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -16,21 +13,13 @@ import org.bukkit.event.Listener;
 public class CraftTranslateListener implements Listener {
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
-    public void onCraftTranslated(final CraftTranslateEvent event) {
+    public void onCraftTranslated(final CraftFinishMovementEvent event) {
         Craft craft = event.getCraft();
         if (craft == null) {
             return;
         }
-        final World world = craft.getWorld();
-        for (MovecraftLocation movecraftLocation : craft.getHitBox().asSet()) {
-            Block block = movecraftLocation.toBukkit(world).getBlock();
-            BlockState state = block.getState();
-            if (state instanceof Banner banner) {
-                if (banner.getPatterns().size() > 0) {
-                    banner.update(false, false);
-                }
-            }
-        }
+
+        WorldManager.INSTANCE.submit(new UpdateBannersTask(craft));
     }
 
     @EventHandler(ignoreCancelled = true)
---
+++ Windfarer/src/main/java/net/countercraft/movecraft/processing/tasks/UpdateBannersTask.java
@@ -0,0 +1,57 @@
+package net.countercraft.movecraft.processing.tasks;
+
+import net.countercraft.movecraft.MovecraftLocation;
+import net.countercraft.movecraft.craft.Craft;
+import net.countercraft.movecraft.processing.effects.Effect;
+import net.countercraft.movecraft.processing.functions.Result;
+import net.countercraft.movecraft.util.BlockCollectionUtil;
+import org.bukkit.World;
+import org.bukkit.block.Banner;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Supplier;
+
+public class UpdateBannersTask implements Supplier<Effect>, Effect {
+
+    protected final World world;
+    protected final List<MovecraftLocation> updateLocations = new ArrayList<>();
+    protected final Craft craft;
+
+    public UpdateBannersTask(Craft craft) {
+        this.world = craft.getWorld();
+        this.craft = craft;
+    }
+
+    @Override
+    public Effect get() {
+        Set<MovecraftLocation> banners = BlockCollectionUtil.getLocations(this.craft, (location, world, craftTmp) -> {
+            BlockState state = world.getState(location);
+            if (state instanceof Banner banner) {
+                if (banner.getPatterns().size() > 0) {
+                    return Result.succeed();
+                }
+            }
+            return Result.fail();
+        });
+        this.updateLocations.addAll(banners);
+        return this;
+    }
+
+    @Override
+    public void run() {
+        boolean processing = !this.craft.isNotProcessing();
+        this.craft.setProcessing(true);
+        for (MovecraftLocation location : this.updateLocations) {
+            Block block = location.toBukkit(this.world).getBlock();
+            BlockState state = block.getState();
+            if (state instanceof Banner banner) {
+                banner.update(false, false);
+            }
+        }
+        this.craft.setProcessing(processing);
+    }
+}
