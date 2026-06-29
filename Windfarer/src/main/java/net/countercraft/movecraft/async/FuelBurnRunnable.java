fdd6f563c6d9ca03d4fc86fe47fa7d29a289fe90
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2026-06-29 20:49:03 +0200

    add legacy support code

--- Windfarer/src/main/java/net/countercraft/movecraft/async/FuelBurnRunnable.java
+++ Windfarer/src/main/java/net/countercraft/movecraft/async/FuelBurnRunnable.java
@@ -1,19 +1,36 @@
 package net.countercraft.movecraft.async;
 
 import com.google.common.collect.Lists;
+import net.countercraft.movecraft.TrackedLocation;
 import net.countercraft.movecraft.craft.Craft;
 import net.countercraft.movecraft.craft.CraftManager;
+import net.countercraft.movecraft.craft.datatag.CraftDataTagKey;
+import net.countercraft.movecraft.craft.datatag.CraftDataTagRegistry;
 import net.countercraft.movecraft.craft.type.PropertyKeys;
+import net.countercraft.movecraft.features.fuel.FuelDataTags;
 import net.countercraft.movecraft.features.fuel.FuelUtil;
 import net.countercraft.movecraft.processing.WorldManager;
 import net.countercraft.movecraft.processing.tasks.FuelBurnTask;
+import net.countercraft.movecraft.processing.tasks.UpdateFuelBurnersTask;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.List;
+import java.util.Set;
 
 // TODO: Migrate to processing system and run async if possible!
 public class FuelBurnRunnable implements Runnable {
     // FuelBurnRate: How much fuel gets burnt per tick?
     // "BurningFuel": How many ticks of fuel does the craft still have aboard?
+
+    @Deprecated(forRemoval = true)
+    public static final CraftDataTagKey<Boolean> IS_FUELED = FuelDataTags.IS_FUELED;
+    @Deprecated(forRemoval = true)
+    public static final CraftDataTagKey<Double> FUEL_PERCENTAGE = FuelDataTags.FUEL_PERCENTAGE;
+    @Deprecated(forRemoval = true)
+    public static final CraftDataTagKey<ItemStack> CURRENT_FUEL_ITEM = FuelDataTags.CURRENT_FUEL_ITEM;
+    @Deprecated(forRemoval = true)
+    public static final CraftDataTagKey<Set<TrackedLocation>> FURNACES = CraftDataTagRegistry.INSTANCE.registerTagKey(FuelDataTags.FURNACES_KEY, FuelUtil::getFuelBurners);
+
 
     @Override
     public void run() {
@@ -29,6 +46,16 @@ public class FuelBurnRunnable implements Runnable {
 
             runFuelBurnLogic(craft, false);
         }
+    }
+
+    @Deprecated(forRemoval = true)
+    public static boolean doesBurnFuel(final Craft craft) {
+       return FuelUtil.doesBurnFuel(craft);
+    }
+
+    @Deprecated(forRemoval = true)
+    public static void updateFurnaces(Craft craft, boolean active) {
+        WorldManager.INSTANCE.submit(new UpdateFuelBurnersTask(craft, active));
     }
 
     public static void runFuelBurnLogic(Craft craft, boolean isStick) {
