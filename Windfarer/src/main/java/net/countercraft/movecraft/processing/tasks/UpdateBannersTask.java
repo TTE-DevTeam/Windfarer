2800ebdd2fca4e7fb848f0189108b9a77e8ee070
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2026-07-05 15:51:52 +0200

    migrate to FilteredTrackedLocation datatag

--- Windfarer/src/main/java/net/countercraft/movecraft/processing/tasks/UpdateBannersTask.java
+++ Windfarer/src/main/java/net/countercraft/movecraft/processing/tasks/UpdateBannersTask.java
@@ -2,12 +2,17 @@ package net.countercraft.movecraft.processing.tasks;
 
 import net.countercraft.movecraft.Movecraft;
 import net.countercraft.movecraft.MovecraftLocation;
+import net.countercraft.movecraft.TrackedLocation;
 import net.countercraft.movecraft.craft.Craft;
+import net.countercraft.movecraft.craft.datatag.CraftDataTagKey;
+import net.countercraft.movecraft.craft.datatag.CraftDataTagRegistry;
+import net.countercraft.movecraft.processing.MovecraftWorld;
 import net.countercraft.movecraft.processing.effects.Effect;
 import net.countercraft.movecraft.processing.functions.Result;
-import net.countercraft.movecraft.util.BlockCollectionUtil;
+import net.countercraft.movecraft.util.FilteredTrackedLocations;
 import net.countercraft.movecraft.util.Tags;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.World;
 import org.bukkit.block.Banner;
 import org.bukkit.block.Block;
@@ -15,16 +20,18 @@ import org.bukkit.block.BlockState;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 import java.util.function.Supplier;
 
-// TODO: Add cache for banners
-// TODO: Write generalized variant for caching blocks via trackedlocations with lifetime
+// DONE: Add cache for banners
+// DONE: Write generalized variant for caching blocks via trackedlocations with lifetime
 public class UpdateBannersTask implements Supplier<Effect>, Effect {
 
     protected final World world;
     protected final List<MovecraftLocation> updateLocations = new ArrayList<>();
     protected final Craft craft;
+
+    private static final NamespacedKey BANNER_LOCATIONS_KEY = new NamespacedKey("windfarer", "tracked_locations/banners");
+    protected static final CraftDataTagKey<FilteredTrackedLocations> BANNER_LOCATIONS = CraftDataTagRegistry.INSTANCE.registerTagKey(BANNER_LOCATIONS_KEY, c -> new FilteredTrackedLocations(c, BANNER_LOCATIONS_KEY, 20000, UpdateBannersTask::isBannerBlock));
 
     public UpdateBannersTask(Craft craft) {
         this.world = craft.getWorld();
@@ -35,22 +42,26 @@ public class UpdateBannersTask implements Supplier<Effect>, Effect {
     public Effect get() {
         final long startTime = System.currentTimeMillis();
         Movecraft.getInstance().getLogger().info(String.format("Starting banner update task for craft <%s>...", craft.getUUID()));
-        Set<MovecraftLocation> banners = BlockCollectionUtil.getLocations(this.craft, (location, world, craftTmp) -> {
-            Material material = world.getMaterial(location);
-            if (Tags.BANNERS.contains(material)) {
-                return Result.fail();
-            }
-            BlockState state = world.getState(location);
-            if (state instanceof Banner banner) {
-                if (banner.getPatterns().size() > 0) {
-                    return Result.succeed();
-                }
-            }
-            return Result.fail();
-        });
-        this.updateLocations.addAll(banners);
+        FilteredTrackedLocations banners = this.craft.getDataTag(BANNER_LOCATIONS);
+        for (TrackedLocation trackedLocation : banners) {
+            this.updateLocations.add(trackedLocation.getAbsoluteLocation());
+        }
         Movecraft.getInstance().getLogger().info(String.format("Finished banner update task for craft <%s>! Time taken: %dms", craft.getUUID(), System.currentTimeMillis() - startTime));
         return this;
+    }
+
+    protected static Result isBannerBlock(MovecraftLocation location, MovecraftWorld world, Craft craftTmp) {
+        Material material = world.getMaterial(location);
+        if (Tags.BANNERS.contains(material)) {
+            return Result.fail();
+        }
+        BlockState state = world.getState(location);
+        if (state instanceof Banner banner) {
+            if (banner.getPatterns().size() > 0) {
+                return Result.succeed();
+            }
+        }
+        return Result.fail();
     }
 
     @Override
