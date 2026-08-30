eb07542441da89cbd6c9ea00f733a2ffd4cefcb1
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2026-08-30 20:21:20 +0200

    Movecraft => Windfarer

--- Windfarer/src/main/java/net/countercraft/movecraft/util/ChatUtils.java
+++ Windfarer/src/main/java/net/countercraft/movecraft/util/ChatUtils.java
@@ -8,7 +8,7 @@ import org.jetbrains.annotations.NotNull;
 
 public class ChatUtils {
     @Deprecated(forRemoval = true)
-    public static final String MOVECRAFT_COMMAND_PREFIX = ChatColor.GOLD + "[" + ChatColor.WHITE + "Movecraft" + ChatColor.GOLD + "] " + ChatColor.RESET;
+    public static final String MOVECRAFT_COMMAND_PREFIX = ChatColor.GOLD + "[" + ChatColor.WHITE + "Windfarer" + ChatColor.GOLD + "] " + ChatColor.RESET;
     @Deprecated(forRemoval = true)
     public static final String ERROR_PREFIX = ChatColor.RED +"[" + I18nSupport.getInternationalisedString("Error") + "]" + ChatColor.RESET;
 
