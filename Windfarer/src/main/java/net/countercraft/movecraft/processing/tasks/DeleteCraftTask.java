eb596d5d1d8c577488620c4351a1412dff2ba80e
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2026-09-01 18:06:07 +0200

    organize imports

--- Windfarer/src/main/java/net/countercraft/movecraft/processing/tasks/DeleteCraftTask.java
+++ Windfarer/src/main/java/net/countercraft/movecraft/processing/tasks/DeleteCraftTask.java
@@ -1,19 +1,15 @@
 package net.countercraft.movecraft.processing.tasks;
 
 import net.countercraft.movecraft.MovecraftLocation;
-import net.countercraft.movecraft.craft.Craft;
 import net.countercraft.movecraft.processing.CachedMovecraftWorld;
 import net.countercraft.movecraft.processing.MovecraftWorld;
 import net.countercraft.movecraft.processing.effects.Effect;
 import net.countercraft.movecraft.processing.effects.SetBlockEffect;
-import net.countercraft.movecraft.processing.functions.Result;
-import net.countercraft.movecraft.util.BlockCollectionUtil;
 import net.countercraft.movecraft.util.hitboxes.BitmapHitBox;
 import net.countercraft.movecraft.util.hitboxes.HitBox;
 import org.bukkit.World;
 import org.bukkit.block.BlockType;
 import org.bukkit.block.data.BlockData;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.Iterator;
 import java.util.function.Supplier;
